8de28c4e88d4f39328c32f2c5600c17c97ce975f
ernestsoo22 <ernestsoo22@gmail.com>
2017-10-19 01:27:40 +0800

    Added-register-page
    Functional registration page to be used with James' PHP script.

--- framework/js/main.js
+++ framework/js/main.js
@@ -2,7 +2,7 @@
 /*global angular */
 /*jslint plusplus:true*/
 var app = angular.module("mainApp",['ngRoute']);
-app.controller("mainCtrl", function($scope)
+app.controller("mainCtrl", function($scope,$window,$interval)
 {
     "use strict";
     
@@ -14,6 +14,69 @@ app.controller("mainCtrl", function($scope)
 
     };
     
+   
+    
+      $scope.warningArray = new Array();
+             
+        $scope.enableWarning = function(num)
+        {
+            $scope.warningArray[num] = true;
+        }
+    
+       $scope.disableWarning =function(num)
+        {
+            $scope.warningArray[num] = false;
+            
+        }
+        
+   
+
+
+
+     var alwaysCheckForm = function()
+    {
+       
+        /** Section 1 **/
+        $window.email = $scope.registerform.email.$valid;
+        $window.password = $scope.registerform.password.$valid;
+        if($scope.password == $scope.repassword)
+        {
+        $window.repassword = $scope.registerform.repassword.$valid;
+        }
+        
+        /** Section 2 **/
+        $window.first = $scope.registerform2.first.$valid;
+        $window.last = $scope.registerform2.last.$valid;
+        $window.tel = $scope.registerform2.tel.$valid;
+        
+        /** Section 3 **/
+        $window.address1 = $scope.registerform3.address1.$valid;
+        $window.address2 = $scope.registerform3.address2.$valid;
+        $window.postcode = $scope.registerform3.postcode.$valid;
+        $window.city = $scope.registerform3.city.$valid;
+        $window.state = $scope.registerform3.state.$valid;
+        $window.country = $scope.registerform3.country.$valid;
+        
+        
+        
+        
+        $window.warning0 = $scope.warningArray[0];
+        $window.warning1 = $scope.warningArray[1];
+        $window.warning2 = $scope.warningArray[2];
+        $window.warning3 = $scope.warningArray[3];
+        $window.warning4 = $scope.warningArray[4];
+        $window.warning5 = $scope.warningArray[5];
+        $window.warning6 = $scope.warningArray[6];
+        $window.warning7 = $scope.warningArray[7];
+        $window.warning8 = $scope.warningArray[8];
+        $window.warning9 = $scope.warningArray[9];
+        $window.warning10 = $scope.warningArray[10];
+        $window.warning11 = $scope.warningArray[11];
+
+
+    }
+ 
+    setInterval(alwaysCheckForm,50);
 });
 
 app.controller("loginCtrl",function($scope, $location, $cookies, $http)
@@ -96,6 +159,9 @@ app.config(['$routeProvider', function($routeProvider)
     $routeProvider
     .when('/home', {
         templateUrl: 'template/home-unregistered.html'
+    })
+    .when('/register', {
+        templateUrl: 'template/register.html'
     })
     .otherwise({
         redirectTo: '/home'
