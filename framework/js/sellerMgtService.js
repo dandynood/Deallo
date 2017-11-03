23a40a4f994af0ad1717dbce007994c3fbe8be83
ernestsoo22 <ernestsoo22@gmail.com>
2017-11-03 22:20:41 +0800

    Added ng-repeat implementation for "My Active Merchandize"

--- framework/js/sellerMgtService.js
+++ framework/js/sellerMgtService.js
@@ -6,6 +6,7 @@ angular.module('sellerMgtService',[])
 {
     "use strict";
     $scope.accountID = 1;
+    $scope.sellerProducts = null;
     
     $scope.getSellersProducts = function(){
     var str = {accountID: encodeURIComponent($scope.accountID)};
@@ -15,11 +16,14 @@ angular.module('sellerMgtService',[])
            $scope.noResult = "Server Error: We unfortunately can't retrieve your products!";
            alert($scope.noResult);
        } else {
-           $scope.sellerProducts = response.data;
-           console.log($scope.sellerProducts);
+           $scope.sellerProducts = response.data[0];
+           console.log(response.data[0]);
+           return response.data[0];
+        //   console.log($scope.sellerProducts[0]);
+      
        }});
     };
     
-    $scope.getSellersProducts();
-    
+    console.log($scope.getSellersProducts());
+    console.log($scope.sellerProducts);
 });
