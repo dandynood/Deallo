fd5e22ea83405a3dc335529db2e4a150911b602f
dandynood <100075176@students.swinburne.edu.my>
2017-11-04 21:57:33 +0800

    add editing php scripts for user profile

---
+++ php/editPassword.php
@@ -0,0 +1,28 @@
+<?php
+    require_once 'config.php';
+    $conn = new mysqli($DB_host,$DB_user,$DB_pass,$DB_name);
+
+    if($conn->connect_error){
+        echo "Connection failed: " . $conn->connect_error;
+        
+    }         
+        $data = json_decode(file_get_contents("php://input"));
+        $password = urldecode($data->password);
+        $accountID = urldecode($data->accountID)
+            
+        $sql = "UPDATE userAccounts SET password='$password' WHERE accountID='$accountID'";
+
+
+        $result = $conn->query($sql);
+
+        if($conn->affected_rows > 0)
+        {
+            echo 'success';
+        }
+        else
+        {
+            echo 'failed';
+        }
+
+    $conn->close();
+?>
