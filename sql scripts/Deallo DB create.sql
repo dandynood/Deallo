9c7044ff2f78d27923fd014c2322518db1d8cb9e
Jacky Ten <yuehan1997@gmail.com>
2017-11-08 13:23:58 +0800

    Add-upload-image-functonality

--- sql scripts/Deallo DB create.sql
+++ sql scripts/Deallo DB create.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE deallo;
+﻿CREATE DATABASE deallo;
 USE deallo;
 
 -- -----------------------------------------------------
@@ -164,6 +164,16 @@ PRIMARY(accountID, productID)
 FOREIGN KEY(productID) REFERENCES products(productID),
 FOREIGN KEY(accountID) REFERENCES useraccounts(accountID)
 );
+
+CREATE TABLE images_tbl(
+   img_id INT (255),
+   img_name VARCHAR(500),
+   img_path VARCHAR(200) NOT NULL,
+   img_type VARCHAR(500),
+   PRIMARY KEY (images_id)
+);
+
+
 
 
 
---
+++ sql scripts/testscript.sql
@@ -0,0 +1,7 @@
+CREATE TABLE images_tbl(
+   img_id INT (255),
+   img_name VARCHAR(500),
+   img_path VARCHAR(200) NOT NULL,
+   img_type VARCHAR(500),
+   PRIMARY KEY (images_id)
+);
